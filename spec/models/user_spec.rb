228ba17288df3cab1062cfb2f7b935352fd77bd7
Alyque Ajani <alyque@gmail.com>
2012-05-27 21:05:24 -0500

    Make a basic user authentication system

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,28 @@ require 'spec_helper'
 
 describe User do
 
-	before { @user = User.new(name: "Example user", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
-	subject { @user }
+  subject { @user }
 
-	it {should respond_to(:name)}
-	it {should respond_to(:email)}
-	it {should respond_to(:password_digest)}
-	it {should respond_to(:password)}
-	it {should respond_to(:password_confirmation)}
-	it { should respond_to(:authenticate) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
-	  it { should be_valid }
+  it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
@@ -24,12 +31,7 @@ describe User do
     before { @user.email = " " }
     it { should_not be_valid }
   end
-
-  describe "when name is too long" do
-  	before {@user.name = "a" * 26}
-  	it {should_not be_valid}
-  end
-
+  
 describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
@@ -84,6 +86,8 @@ describe "when email format is invalid" do
   	 before { @user.password_confirmation = "mismatch" }
   	 it { should_not be_valid }
   end
+
+  it { should respond_to(:authenticate) }
 
   describe "return value of authenticate method" do
     before { @user.save }
